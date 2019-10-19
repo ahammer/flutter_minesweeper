dd8fbfe4411186517d33a79165ec9c5fce54d2b2
Adam Hammer <adamhammer@gmail.com>
2019-10-18 23:37:09 -0700

    Added mine generation logic

--- lib/src/state/actions/minesweeper_actions.dart
+++ lib/src/state/actions/minesweeper_actions.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:mine_sweeper/src/scaffolding/mine_redux.dart';
 import 'package:mine_sweeper/src/state/app_state.dart';
 
@@ -15,8 +17,40 @@ class TouchMineSweeperTileAction extends Reducer {
             //Index position of the node
             final nodeIdx = y * oldState.mineSweeper.width + x;
             final node = oldState.mineSweeper.nodes[nodeIdx];
-            final newNode = node.rebuild((b) => b..isVisible = true);
+            final newNode = node.rebuild((b) => b..isVisible = true);            
             b.mineSweeper.nodes[nodeIdx] = newNode;
+
+            //No bombs yet, lets assign now
+            if (newNode.isBomb == null) {
+              assignBombs(b);
+            }
             return b;
           });
+}
+
+void assignBombs(AppStateBuilder b) {
+  final nodes = b.mineSweeper.nodes;
+  final bombCount = b.mineSweeper.bombs;
+  final width = b.mineSweeper.width;
+  final height = b.mineSweeper.height;
+  final random = Random();
+
+  int remaining = bombCount;
+  
+  //Set all the bombs to false
+  for (int i=0;i<width*height;i++) {
+    nodes[i] = nodes[i].rebuild((b)=>b..isBomb = false);
+  }
+
+  //Assign bombCount at random
+  while (remaining > 0) {
+    int x = random.nextInt(width);
+    int y = random.nextInt(height);
+    final idx = x+y*width;
+    final node = nodes[idx];
+    if (node.isVisible != true && node.isBomb != true) {    
+      nodes[idx] = node.rebuild((b)=>b..isBomb = true);
+      remaining--;
+    }
+  }
 }
--- test/minesweeper_test.dart
+++ test/minesweeper_test.dart
@@ -14,7 +14,14 @@ main() {
     expect(appState.mineSweeper.getNode(x:4, y:4).isVisible, equals(false));
 
     final AppState newState = TouchMineSweeperTileAction(x:4,y:4).reduce(appState);
-    expect(newState.mineSweeper.getNode(x:4, y:4).isVisible, equals(true));
-    
+    expect(newState.mineSweeper.getNode(x:4, y:4).isVisible, equals(true));    
+  });
+
+  test("Test mine generation", () {
+    final AppState appState = AppState.getDefault();
+    //Fake a touch
+    final AppState newState = TouchMineSweeperTileAction(x:4,y:4).reduce(appState);
+    int bombCount = newState.mineSweeper.nodes.fold(0, (value, node) => value + (node.isBomb?1:0));
+    expect(bombCount, equals(newState.mineSweeper.bombs));
   });
 }
